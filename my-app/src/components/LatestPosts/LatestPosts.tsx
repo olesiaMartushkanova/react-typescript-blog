44904a7ba9c49ec5e4df733b7950b6e45bf3151a
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-04-24 22:16:48 +1200

    styled latest posts container

--- my-app/src/components/LatestPosts/LatestPosts.tsx
+++ my-app/src/components/LatestPosts/LatestPosts.tsx
@@ -1,7 +1,11 @@
 import './LatestPosts.css';
 
 const LatestPosts = () => {
-  return <div className='latestsPosts'>Latest posts container</div>;
+  return (
+    <div className='latestsPosts'>
+      <h3 className='title'>My latest posts</h3>
+    </div>
+  );
 };
 
 export default LatestPosts;
