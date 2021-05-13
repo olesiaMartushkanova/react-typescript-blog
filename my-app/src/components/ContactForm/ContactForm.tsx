5b9715d48e4d09101447f8e90479599e13d4b3a5
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-05-13 21:09:39 +1200

    validation is working

--- my-app/src/components/ContactForm/ContactForm.tsx
+++ my-app/src/components/ContactForm/ContactForm.tsx
@@ -1,6 +1,6 @@
+import { SyntheticEvent, useState } from 'react';
 import './ContactForm.css';
 import SendButton from './SendButton';
-import { useForm } from 'react-hook-form';
 import { sendEmail } from '../../utils/sendEmail';
 
 interface IContactForm {
@@ -8,62 +8,79 @@ interface IContactForm {
   className?: string;
 }
 
-type FormValues = {
-  name: string;
-  email: string;
-  message: string;
-};
-
 const ContactForm = (props: IContactForm) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
+  const [formData, setFormData] = useState({} as any);
+  const [formErrors, setFormErrors] = useState({} as any);
 
-  const onSubmit = (data: FormValues, e: any) => {
-    console.log(data);
+  const onSubmit = (e: any) => {
     e.preventDefault();
-    sendEmail(e.target);
-    e.target.reset();
+
+    const newFormErrors: any = {};
+    if (!formData['name']) {
+      newFormErrors['name'] = 'Please, leave your name';
+    }
+
+    if (!formData['email']) {
+      newFormErrors['email'] = 'Please, leave your email';
+    }
+
+    if (!formData['message']) {
+      newFormErrors['message'] = 'Please, leave your message';
+    }
+
+    if (Object.keys(newFormErrors).length === 0) {
+      // sendEmail(e.target);
+      e.target.reset();
+      setFormData({});
+      console.log('Sent');
+    }
+
+    setFormErrors(newFormErrors);
+  };
+
+  const setFormValue = (name: string, event: any) => {
+    const newFormData = formData;
+    newFormData[name] = event.target.value;
+    setFormData(newFormData);
   };
 
   return (
     <div className={`contactFormContainer ${props.className}`}>
       <label>Contact Me</label>
       <div>
-        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <form noValidate onSubmit={onSubmit}>
           <input
-            {...register('name', {
-              required: 'Please, leave your name.',
-              maxLength: 26,
-              minLength: 2,
-            })}
             type='text'
             placeholder='Your Name'
             id='name'
             name='name'
             className='input contactField'
+            onChange={(event) => setFormValue('name', event)}
           />
-          {errors.name && <p className='error'>{errors.name.message}</p>}
-          {errors.name && <p className='error'>{errors.name.message}</p>}
+          {formErrors['name'] && <p className='error'>{formErrors['name']}</p>}
 
           <input
-            {...register('email', { required: 'please, leave your email.' })}
-            placeholder='Your Email address'
             id='email'
             name='email'
             type='email'
+            placeholder='Your Email address'
             className='input contactField email'
+            onChange={(event) => setFormValue('email', event)}
           />
-          {errors.email && <p className='error'>{errors.email.message}</p>}
+          {formErrors['email'] && (
+            <p className='error'>{formErrors['email']}</p>
+          )}
 
           <textarea
-            {...register('message')}
             id='message'
             name='message'
             className='message'
-            placeholder={`If you send me a message\nI will reply you in the next couple of days.`}></textarea>
+            placeholder={`If you send me a message\nI will reply you in the next couple of days.`}
+            onChange={(event) => setFormValue('message', event)}></textarea>
+          {formErrors['message'] && (
+            <p className='error'>{formErrors['message']}</p>
+          )}
+
           <SendButton />
         </form>
       </div>
