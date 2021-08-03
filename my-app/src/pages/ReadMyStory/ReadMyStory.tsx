a10b33d29bc0932db9e83263cd3cbd2a692efc64
olesiaMartushkanova <olesia.martushkanova@seeeverything.com>
2021-08-04 09:07:46 +1200

    inserted images

--- my-app/src/pages/ReadMyStory/ReadMyStory.tsx
+++ my-app/src/pages/ReadMyStory/ReadMyStory.tsx
@@ -1,6 +1,12 @@
 import './ReadMyStory.css';
 import Page from '../../components/Page/Page';
 import { HOME_PATH } from '../../utils/constants';
+import SEO from '../../images/SEO.jpg';
+import Team from '../../images/Team.jpg';
+import GlobalLogic from '../../images/Global-Logic.jpg';
+import Facebook from '../../images/Facebook.jpg';
+import Regent from '../../images/Regent.jpg';
+import Warehouse from '../../images/Warehouse.jpg';
 
 const ReadMyStory = () => {
   return (
@@ -20,6 +26,12 @@ const ReadMyStory = () => {
             graduated from university, went on 3 months trip to Asia and decided
             that teaching wasn’t something I wanted to do all my life.
           </p>
+
+          <div className='img__container'>
+            <img className='img__single' src={SEO}></img>
+            <div className='img__text'>My first $10 for SEO work</div>
+          </div>
+
           <p>
             It was a hard time for me, I felt really down: 5 years of my life
             were wasted on getting the education that I didn’t want to use.
@@ -40,6 +52,7 @@ const ReadMyStory = () => {
             step in my life: sign up in ‘Go IT’ for the ‘Quality Assurance’
             3-months course. I told myself: why not try?
           </p>
+
           <p>
             It’s probably easy to guess that I really loved to study there! It
             was a whole new world for me and I was going to be the best student.
@@ -54,6 +67,26 @@ const ReadMyStory = () => {
             1st of August, 2016: I have started to work as a Software Test
             Engineer. (Big day!)
           </p>
+
+          <div className='img__container-row'>
+            <div className='img__container-row__with-text'>
+              <img className='img__team' src={Team}></img>
+              <div className='img__text'>My team in GlobalLogic</div>
+            </div>
+
+            <div className='img__container-row__with-text'>
+              <img className='img__team' src={Facebook}></img>
+              <div className='img__text'>
+                Official first day as Software Tester
+              </div>
+            </div>
+
+            <div className='img__container-row__with-text'>
+              <img className='img__team' src={GlobalLogic}></img>
+              <div className='img__text'>Our office in GlobalLogic</div>
+            </div>
+          </div>
+
           <p>
             I still remember my first day, I don’t think I have ever been so
             scared! Nevertheless, I was so lucky with my first team and my first
@@ -63,6 +96,12 @@ const ReadMyStory = () => {
             IT! I was very happy that I found something that I was enjoying
             doing (and I still enjoy it!).
           </p>
+
+          <div className='img__container'>
+            <img className='img__single' src={Regent}></img>
+            <div className='img__text'>My first day of studying in NZ</div>
+          </div>
+
           <p>
             In 2017 I have moved to beautiful New Zealand. I came here to study
             Computer science post-graduation diploma. After one month in New
@@ -76,6 +115,12 @@ const ReadMyStory = () => {
             automation with C#, with Java and then with JavaScript. I loved
             coding, the more I was learning, the more I wanted to learn.
           </p>
+
+          <div className='img__container-warehouse'>
+            <img className='img__single' src={Warehouse}></img>
+            <div className='img__text'>In The Warehouse Group: Christmas</div>
+          </div>
+
           <p>
             Today I keep focusing on test automation. I love it, I love solving
             challenges, I love how creative I can be at this job. But I also
